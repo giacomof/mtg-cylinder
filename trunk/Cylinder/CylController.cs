59b26ceb8327ea763b5e408aaa8f88d427834f03
telivyel@gmail.com <telivyel@gmail.com@2c6ae97e-1bdd-0a9e-5a12-75cb8d5785f3>
2011-03-17 13:56:51 +0000

    commented unused inputs

--- trunk/Cylinder/CylController.cs
+++ trunk/Cylinder/CylController.cs
@@ -63,17 +63,17 @@ public class CylController : MonoBehaviour
         if (Input.GetButtonDown("Fire2"))	print("Button 2");
         if (Input.GetButtonDown("Fire3"))	print("Button 3");
         if (Input.GetButtonDown("Jump"))	print("Button 4");
-        if (Input.GetButtonDown("Fire4"))	print("Button 5");
-        if (Input.GetButtonDown("Fire5"))	print("Button 6");
-        if (Input.GetButtonDown("Fire6"))	print("Button 7");
+        //if (Input.GetButtonDown("Fire4"))	print("Button 5");
+        //if (Input.GetButtonDown("Fire5"))	print("Button 6");
+        //if (Input.GetButtonDown("Fire6"))	print("Button 7");
         if (Input.GetButtonDown("Fire7"))	//print("Button 8");
 			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         if (Input.GetButtonDown("Fire8"))	print("Button 9");
         if (Input.GetButtonDown("Fire9"))	//print("Button 10");
             Application.LoadLevel(0);
         //}
-		if (Input.GetButtonDown("Fire10"))	print("Button 11");
-		if (Input.GetButtonDown("Fire11"))	print("Button 12");
+		//if (Input.GetButtonDown("Fire10"))	print("Button 11");
+		//if (Input.GetButtonDown("Fire11"))	print("Button 12");
         //else if (cylinder.controlScheme == CylController.ControlScheme.XBoxController)
     }
 	
